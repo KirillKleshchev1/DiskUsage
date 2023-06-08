b0d34f04896f6cb8337488e7b7a9bf23083a441c
Kirill.Kleshchev1 <theobject705@gmail.com>
2023-06-08 19:25:17 +0500

    upp

--- diskSpaceLogic.py
+++ diskSpaceLogic.py
@@ -5,6 +5,10 @@ class DiskSpaceLogic:
     def __init__(self, path: str):
         self.path = path
 
+    """
+    Функция для получения информации о диске.
+    Возвращает кортеж, содержащий размер диска, используемое место на диске, свободное место на диске
+    """
     def get_disk_usage(self) -> tuple:
         usage = shutil.disk_usage(self.path)
         print(usage)
